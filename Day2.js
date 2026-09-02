beed98f34e6ec18b77af8eee2e2c887b9a33e852
vikrant <vikrant2003vikrant@gmail.com>
2026-09-02 12:53:40 +0530

    Add conditional statements and enhance array manipulation examples in Day2.js

--- Day2.js
+++ Day2.js
@@ -66,3 +66,63 @@ let heros = ["a", "b", "asdf", true, "123", 123, ,];
 for (let i = 0; i < heros.length; i++) {
   console.log(heros[i], typeof heros[i]);
 }
+
+//conditional statements
+let num1 = 2;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log(num1, ` is grater than `, num2);
+} else {
+  console.log(num2, ` is grater than `, num1);
+}
+
+let name = "vikrant";
+let name2 = "vikrant";
+
+console.log(name === name2 ? `they are same` : `they are not same`);
+
+let login = true;
+console.log(login == true ? `welcome to site` : `need to login`);
+
+let arr = ["asdf"];
+console.log(arr.length ? `Array is not empty` : `Array is empty`);
+
+let teaFlavors = ["green tea", "black tea", "oolong tea"];
+let firstTea = teaFlavors[0];
+console.log(firstTea);
+
+let cities = ["London", "Tokya", "Paris", "New york"];
+let favoriteCity = cities[2];
+console.log(favoriteCity);
+
+let teaTypes = ["herbal tea", "white tea", "masala chai"];
+teaTypes[1] = 'jasmine tea';
+console.log(teaTypes[1]);
+
+let citiesVisited = ['mumbai','sydeny'];
+citiesVisited.push = 'berlin'
+console.log(citiesVisited);
+
+let teaOrdera = ['chai','iced tea','earl grey'];
+// teaOrdera.pop = 'earl grey'
+console.log(teaOrdera);
+
+let softCopy = teaOrdera;
+console.log(softCopy);
+// teaOrdera.pop();
+
+let hardCopy = [...teaOrdera]
+// hardCopy.pop();
+console.log(hardCopy);
+
+
+
+let totalOrder = [teaFlavors + teaOrdera];
+let newtotalOrder = teaFlavors.concat(teaOrdera);
+console.log(typeof totalOrder);
+console.log(typeof newtotalOrder);
+console.log(totalOrder);
+let length = newtotalOrder.length;
+console.log(length);
+
