cf5cc73b0190aeb7cc7ca5abcc9f65aedbb4b2b2
vikrant <vikrant2003vikrant@gmail.com>
2026-09-06 17:27:52 +0530

    DOM manipulation

---
+++ Day6/app.js
@@ -0,0 +1,44 @@
+//DOM manipulation by javaScript
+//document object model
+let btn = document.querySelector("#changeTextButton");
+let para = document.querySelector("#myParagraph");
+btn.addEventListener("click", function () {
+  console.log("you clicked the btn");
+  console.log(this);
+  para.textContent = "i am back";
+});
+
+let btn2 = document.querySelector("#highlightFirstCity");
+
+btn2.addEventListener("click", function () {
+  let btn = document.querySelector("#citiesList .chai");
+  btn.classList.add("highlight");
+  console.log(btn.classList);
+});
+
+//example 3
+document.querySelector("#changeOrder").addEventListener("click", function () {
+  let btn = document.querySelector("#coffeeType");
+  btn.textContent = "Expresso";
+  btn.style.color = "red";
+  btn.style.color = "red";
+  btn.style.backgroundColor = "green";
+  console.log(btn);
+});
+
+//example 4
+document.querySelector("#addNewItem").addEventListener("click", function () {
+  let list = document.querySelector("#taskList");
+  let shopinglist = document.querySelector("#shoppingList");
+  let product = prompt("Enter the item");
+  let newItem = document.createElement("li");
+  newItem.textContent = product;
+  shopinglist.appendChild(newItem);
+});
+//example 5
+function deleteItem() {
+  document
+    .querySelector("#taskList")
+    .removeChild(document.querySelector("#taskList").lastChild);
+}
+document.querySelector("#removeLastTask").addEventListener("click", deleteItem);
