22f23549425cdac3ff09ea8d886d8f22be09bff7
vikrant <vikrant2003vikrant@gmail.com>
2026-09-09 20:44:29 +0530

    Implement task addition and completion functionality in To-Do List

--- Day8/ToDo.js
+++ Day8/ToDo.js
@@ -1,17 +1,43 @@
-let task = [];
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.querySelector("input");
+  let task = JSON.parse(localStorage.getItem("tasks")) || [];
+  task.forEach((task) => {
+    renderTask(task);
+  });
 
-function showAllTask() {
-  for (let i = 0; i < task.length; i++) {
+  document.querySelector(".addTask").addEventListener("click", function () {
+    const taskText = input.value.trim();
+    if (taskText === "") return;
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    task.push(newTask);
+    saveTask();
+    input.value = "";
+  });
+
+  function renderTask(task) {
     let todo = document.createElement("li");
-    todo.value = task[i];
-    let btn = (document.createElement("button").innerText = "Done");
-    todo.append(btn);
+    todo.setAttribute("data-id", task.id);
+    // if(task.completed) todo.classList.add('completed');
+    // todo.addEventListener('click',(e)=>{
+    //   if(e.target.tagName === 'BUTTON') return;
+    //   todo.completed = !task.completed;
+    //   todo.classList.toggle('completed');
+    //   saveTask();
+    // })
+    todo.innerText = task.text;
+    let doneBtn = document.createElement("button");
+    doneBtn.textContent = "Done";
+    todo.append(doneBtn);
     document.querySelector(".list ul").append(todo);
+    todo.addEventListener('click',()=>{
+      todo.classList.toggle('completed');
+    })
   }
-}
-
-document.querySelector(".addTask").addEventListener("click", function () {
-  task.push(document.querySelector("input").value);
-  showAllTask();
-  document.querySelector("input").value = "";
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(task));
+  }
 });
